f6cbe8d294dbe41b79e3e320f52ab114b64cab49
Nhan Kenny DIEP <nhankennydiep@gmail.com>
2021-08-27 09:47:47 +0200

    Update The_RiverI.php

--- Easy Puzzles/PHP/The_RiverI.php
+++ Easy Puzzles/PHP/The_RiverI.php
@@ -3,10 +3,13 @@
 fscanf(STDIN, "%d", $r1);
 fscanf(STDIN, "%d", $r2);
 
+// Tant que les deux nombres sont différents : 
 while ($r1 != $r2){
 
+    // Si le nombre 1 est supérieur au nombre 2
     if ($r1 > $r2){
 
+        // On déclare la somme des chiffres du nombre
         $sumDigit = 0;
 
         for ($i=0; $i < strlen($r2); $i++){
@@ -14,10 +17,12 @@ while ($r1 != $r2){
             $sumDigit += $digitValue;
         } 
 
+        // On l'ajoute au nombre 2
         $r2 += $sumDigit;
 
     } elseif ($r1 < $r2) {
 
+        // On déclare la somme des chiffres du nombre
         $sumDigit = 0;
         
         for ($i=0; $i < strlen($r1); $i++){
@@ -25,9 +30,11 @@ while ($r1 != $r2){
             $sumDigit += $digitValue;
         }
 
+        // On l'ajoute au nombre 1
         $r1 += $sumDigit;
     }
 
 }
 
+// On affiche le nombre 1 (ou le nombre 2)
 echo("$r1\n");
