5f3ef4effc6aa7037573a7b2ef6ce2a7f30cfc90
Nhan Kenny DIEP <nhankennydiep@gmail.com>
2021-08-27 22:21:54 +0200

    Update The_RiverI.php

--- Easy Puzzles/PHP/The_RiverI.php
+++ Easy Puzzles/PHP/The_RiverI.php
@@ -4,28 +4,27 @@ fscanf(STDIN, "%d", $r1);
 fscanf(STDIN, "%d", $r2);
 
 // Tant que les deux nombres sont différents : 
-while ($r1 != $r2){
+while ($r1 != $r2) {
 
     // Si le nombre 1 est supérieur au nombre 2
-    if ($r1 > $r2){
+    if ($r1 > $r2) {
 
         // On déclare la somme des chiffres du nombre
         $sumDigit = 0;
 
-        for ($i=0; $i < strlen($r2); $i++){
+        for ($i = 0; $i < strlen($r2); $i++) {
             $digitValue = substr($r2, $i, 1);
             $sumDigit += $digitValue;
-        } 
+        }
 
         // On l'ajoute au nombre 2
         $r2 += $sumDigit;
-
-    } elseif ($r1 < $r2) {
+    } else {
 
         // On déclare la somme des chiffres du nombre
         $sumDigit = 0;
-        
-        for ($i=0; $i < strlen($r1); $i++){
+
+        for ($i = 0; $i < strlen($r1); $i++) {
             $digitValue = substr($r1, $i, 1);
             $sumDigit += $digitValue;
         }
@@ -33,8 +32,29 @@ while ($r1 != $r2){
         // On l'ajoute au nombre 1
         $r1 += $sumDigit;
     }
-
 }
 
 // On affiche le nombre 1 (ou le nombre 2)
-echo("$r1\n");
+echo ("$r1\n");
+
+
+
+// function iterate($r) {
+//     $nombres = strval($r);
+//     $nombres = str_split($r, 1);
+//     return $r + array_sum($nombres);
+// }
+
+// fscanf(STDIN, "%d", $r1);
+// fscanf(STDIN, "%d", $r2);
+
+// while ($r1 != $r2) {
+//     if ($r1 < $r2) {
+//         $r1 = iterate($r1);
+//     }
+//     else {
+//         $r2 = iterate($r2);
+//     }
+// }
+
+// echo("$r1\n");
